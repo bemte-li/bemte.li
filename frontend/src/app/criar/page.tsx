98a3c9b78d07ebbc50e2018c90a6caa99c70dd34
Luccas Quadros <luccas.quadros@grafana.com>
2024-02-26 09:55:09 -0300

    Add Rodape component to page.tsx and criar/page.tsx

--- frontend/src/app/criar/page.tsx
+++ frontend/src/app/criar/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Editor from '@/components/editor';
+import Rodape from '@/components/rodape';
 import React, { useState } from 'react';
 
 export default function Page() {
@@ -20,18 +21,42 @@ export default function Page() {
         setTitle(e.target.value);
     };
 
+    const handleSendTest = () => {
+        console.log('Send test');
+    };
+
+    const handlePreview = () => {
+        console.log('Preview');
+    };
+
+    const handleSave = () => {
+        console.log('Save');
+    };
+
+    const handleSend = () => {
+        console.log('Send');
+    };
+
     return (
         <div>
-            <label htmlFor="from">From:</label>
-            <input type="text" id="from" value={from} onChange={handleFromChange} />
+            <div>
+                <button onClick={handleSendTest}>Send test</button>
+                <button onClick={handlePreview}>Preview</button>
+                <button onClick={handleSave}>Save</button>
+                <button onClick={handleSend}>Send</button>
+            </div>
+            <div>
+                <label htmlFor="from">From:</label>
+                <input type="text" id="from" value={from} onChange={handleFromChange} />
 
-            <label htmlFor="to">To:</label>
-            <input type="text" id="to" value={to} onChange={handleToChange} />
+                <label htmlFor="to">To:</label>
+                <input type="text" id="to" value={to} onChange={handleToChange} />
 
-            <label htmlFor="title">Title:</label>
-            <input type="text" id="title" value={title} onChange={handleTitleChange} />
-
+                <label htmlFor="title">Title:</label>
+                <input type="text" id="title" value={title} onChange={handleTitleChange} />
+            </div>
             <Editor />
+            <Rodape />
         </div>
     );
 }
---
+++ frontend/src/components/rodape.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Rodape: React.FC = () => {
+    return (
+        <div className="footer">
+            <h3>Rodapé temporário</h3>
+            <p>A definir</p>
+        </div>
+    );
+};
+
+export default Rodape;
